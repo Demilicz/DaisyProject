76863a40ac8de8b554a13b809d4f6a323b3aed14
Demilicz <Necrolick92@gmail.com>
2021-04-20 19:43:15 +0200

    difficult tabulation not full but almost done

--- src/js/tabs.js
+++ src/js/tabs.js
@@ -1,38 +1,43 @@
-const tab1 = document.querySelector('.');
-const tab2 = document.querySelector('.');
-const tab3 = document.querySelector('.');
+const images = document.querySelectorAll('.portfolio__card');
+const colectionsImg = [...images].reduce(imgHandler,[]);
 
-const blocksList = document.querySelectorAll('.');
-const tabsList = document.querySelectorAll('.');
+const tab1 = document.querySelector('._all');
+const tab2 = document.querySelector('._enter');
+const tab3 = document.querySelector('._sport');
+const tab4 = document.querySelector('._business');
+const tab5 = document.querySelector('._build');
+
+const parentTabs = document.querySelector('.portfolio__images');
+
+const blocksList = document.querySelectorAll('.portfolio__card');
+const tabsList = document.querySelectorAll('.portfolio__link');
+
+tab1.addEventListener("click", () => tabsListener("portfolio__card"));
+tab2.addEventListener("click", () => tabsListener("card_enter"));
+tab3.addEventListener("click", () => tabsListener("card_sport"));
+tab4.addEventListener("click", () => tabsListener("card_business"));
+tab5.addEventListener("click", () => tabsListener("card_build"));
 
 
+  // handlers
 
-tab1.addEventListener('click', () => {
-  tabsHandler(tabsList, blocksList, "someclass", "someclass")
-});
+  function imgHandler(acc, img ){
+    acc.push({"classes": img.classList.value, "src" : img.firstChild.alt });
+    return acc;
+  };
 
+  function tabsListener(cls) {
 
+    parentTabs.innerHTML = '';
+    colectionsImg.forEach(element => {
 
-function tabsHandler (tabs, blocks, blockClass, tabClass) {
-  tabs.forEach(tab => {
-    if(tab.classList.contains(tabClass)){
-      tab.classList.add('_active-tab');
+      if(element.classes.includes(cls)) {
+
+      parentTabs.innerHTML +=`<div class="${element.classes}"><img src="${element.src}"></div>`;
+
     }
-    else
-      tab.classList.remove('_active-tab');
   });
-
-  blocks.forEach((block) => {
-    if(block.classList.contains(blockClass)) {
-      block.classList.remove('d-none');
-    }
-    else
-      block.classList.add('d-none');
-  });
- }
-
-
-
+}
 
 
 
